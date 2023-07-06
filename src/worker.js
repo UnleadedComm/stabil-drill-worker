fbde2977fdafa71474be076310be380bd376f080
David Garcia Jr <dgarcia@ulcomm.com>
2023-07-06 09:22:48 -0500

    Continued.

--- src/worker.js
+++ src/worker.js
@@ -8,8 +8,47 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, doc, setDoc } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 export default {
+	// async fetch(request, env, ctx) {
+	// 	return new Response('Hello World!');
+	// },
+
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		// Your web app's Firebase configuration
+		const firebaseConfig = {
+			apiKey: 'AIzaSyBeTgVffJnRmDU2BchZQqXnhB4v13M0-HE',
+			authDomain: 'stabil-drill-services.firebaseapp.com',
+			databaseURL: 'https://stabil-drill-services.firebaseio.com',
+			projectId: 'stabil-drill-services',
+			storageBucket: 'stabil-drill-services.appspot.com',
+			messagingSenderId: '521270126199',
+			appId: '1:521270126199:web:f21367eecc040f31014872',
+		};
+
+		// Initialize Firebase
+		const app = initializeApp(firebaseConfig);
+
+		const db = getFirestore(app);
+
+		console.log(db);
+
+		const collRef = doc(collection(db, 'contact-form-2023'));
+
+		console.log(collRef);
+
+		const result = await setDoc(collRef, {
+			firstName: 'David',
+			lastName: 'Garcia',
+			email: 'dgarcia@ulcomm.com',
+		});
+
+		console.log(result.id);
+
+		return new Response(result);
 	},
 };
